755d76df9713faecf59471f183203f272d0b16c9
Dan Gopstein <dan@gopstein.com>
2013-09-14 13:21:39 -0400

    Move common code to shared DeepEnumerable module

--- lib/deep_enumerable.rb
+++ lib/deep_enumerable.rb
@@ -1,6 +1,6 @@
 ##
 # A set of general methods that can be applied to any conformant nested structure
-class DeepEnumerable
+module DeepEnumerable
   ##
   # Turns a list of parent nodes into an accessor key
   #
@@ -14,24 +14,30 @@ class DeepEnumerable
       array.first
     end
   end
-end
 
-##
-# This class implements the necessary methods to qualify Hash as a DeepEnumerable
-class Hash
   ##
-  # Iterate elements of a deeply nested hash
+  # Iterate elements of a deeply nested structure
   #
   # Example:
-  #   >> {event: {id: 1, title: 'bowling'}}.deep_each
+  #   >> {event: {id: 1, title: 'bowling'}}.deep_each.to_a
   #   => [[{:event=>:id}, 1], [{:event=>:title}, "bowling"]]
-  def deep_each(ancestry=[]) #TODO remove need for ancestry param
-    flat_map do |key, val|
+  # 
+  #   >> [:a, [:b, :c]].deep_each.to_a
+  #   => [[0, :a], [{1=>0}, :b], [{1=>1}, :c]]
+  #
+  #   >> {events: [{title: 'movie'}, {title: 'dinner'}]}.deep_each.to_a
+  #   => [[{:events=>{0=>:title}}, "movie"], [{:events=>{1=>:title}}, "dinner"]]
+  def deep_each(&block)
+    deep_each_depth_first_map.each(&block)
+  end
+
+  def deep_each_depth_first_map(ancestry=[]) #TODO make this private
+    shallow_each.flat_map do |key, val|
       full_ancestry = ancestry + [key]
       full_key = DeepEnumerable.deep_key_from_array(full_ancestry) #TODO this is an n^2 operation
-
-      if val.respond_to?(:deep_each)
-        val.deep_each(full_ancestry)
+ 
+      if val.respond_to?(:deep_each_depth_first_map)
+        val.deep_each_depth_first_map(full_ancestry)
       else
         [[full_key, val]]
       end
@@ -40,24 +46,19 @@ class Hash
 end
 
 ##
+# This class implements the necessary methods to qualify Hash as a DeepEnumerable
+class Hash
+  include DeepEnumerable
+
+  alias_method :shallow_each, :each
+end
+
+##
 # This class implements the necessary methods to qualify Array as a DeepEnumerable
 class Array
-  ##
-  # Iterate elements of a deeply nested array
-  #
-  # Example:
-  #   >> [:a, [:b, :c]].deep_each
-  #   => [[0, :a], [{1=>0}, :b], [{1=>1}, :c]]
-  def deep_each(ancestry=[])
-    each_with_index.flat_map do |val, key|
-      full_ancestry = ancestry + [key]
-      full_key = DeepEnumerable.deep_key_from_array(full_ancestry) #TODO this is an n^2 operation
+  include DeepEnumerable
 
-      if val.respond_to?(:deep_each)
-        val.deep_each(full_ancestry)
-      else
-        [[full_key, val]]
-      end
-    end
+  def shallow_each
+    each_with_index.map{|*a| a.reverse}
   end
 end
--- test/deep_enumerable_test.rb
+++ test/deep_enumerable_test.rb
@@ -1,9 +1,10 @@
+#require 'minitest_helper'
 require 'minitest/autorun'
 require 'deep_enumerable.rb'
 require 'set'
 
-class TestDeepEnumerable < MiniTest::Unit::TestCase
-  def test_deep_key_from_array
+describe DeepEnumerable do
+  it "should map ancestry lists to accessor keys" do
     f = lambda {|x| DeepEnumerable.deep_key_from_array(x)}
     #assert_equal({}, f.call([]), "empty array should make an empty ancestry hash")
     assert_equal(:a, f.call([:a]), "ancestry of size 1")
@@ -12,24 +13,36 @@ class TestDeepEnumerable < MiniTest::Unit::TestCase
   end
 end
 
-class TestDeepHash < MiniTest::Unit::TestCase
-  def test_deep_each
+describe Hash do
+  it "should work with :deep_each" do
     hash = {a: {b: 1, c: {d: 2, e: 3}, f: 4}, g: 5}
-    all_keys = [{:a=>:b}, {:a=>{:c=>:d}}, {:a=>{:c=>:e}}, {:a=>:f}, :g].to_set
-    all_vals = (1..5).to_set
+    keys = [{:a=>:b}, {:a=>{:c=>:d}}, {:a=>{:c=>:e}}, {:a=>:f}, :g].to_set
+    vals = (1..5).to_set
 
-    assert_equal(all_keys, hash.deep_each.map(&:first).to_set, 'deep_each yields fully qualified keys of hash-only nested structures')
-    assert_equal(all_vals, hash.deep_each.map(&:last).to_set, 'deep_each is implemented for hash-only nested structures')
+    test_deep_each(hash, keys, vals)
   end
 end
 
-class TestDeepArray < MiniTest::Unit::TestCase
-  def test_deep_each
+describe Array do
+  it "should work with DeepEnumerable" do
     array = [:a, [:b, [[:c], :d], :e]]
-    all_keys = [0, {1 => 0}, {1 => {1 => {0 => 0}}}, {1 => {1 => 1}}, {1 => 2}].to_set
-    all_vals = (:a..:e).to_set
+    keys = [0, {1 => 0}, {1 => {1 => {0 => 0}}}, {1 => {1 => 1}}, {1 => 2}].to_set
+    vals = (:a..:e).to_set
 
-    assert_equal(all_keys, array.deep_each.map(&:first).to_set, 'deep_each yields fully qualified keys for array-only nested structures')
-    assert_equal(all_vals, array.deep_each.map(&:last).to_set, 'deep_each is implemented for array-only nested structures')
+    test_deep_each(array, keys, vals)
   end
+end
+
+def test_deep_each(object, keys, vals)
+    result_keys = Set.new
+    result_vals = Set.new
+    object.deep_each do |k,v|
+      result_keys << k
+      result_vals << v
+    end
+    assert_equal(keys, result_keys, 'yields fully qualified keys')
+    assert_equal(vals, result_vals, 'yields values')
+
+    assert_equal(keys, object.deep_each.map(&:first).to_set, 'maps fully qualified keys')
+    assert_equal(vals, object.deep_each.map(&:last).to_set, 'maps values')
 end
